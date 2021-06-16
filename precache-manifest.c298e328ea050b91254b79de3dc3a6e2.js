222643e484a95b7c9c431826fc9a47407e38165d
KESHAV GUPTA <iamkeshavgupta27@gmail.com>
2021-06-16 10:57:31 +0530

    Birthday list

---
+++ precache-manifest.c298e328ea050b91254b79de3dc3a6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d7fa03343c1734dc7d79b985aee5fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "427bbc2804ba04f4e49d",
+    "url": "/static/css/main.8eb2907e.chunk.css"
+  },
+  {
+    "revision": "cbc54fe8c7c592cdd1c4",
+    "url": "/static/js/2.2d50357b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2d50357b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427bbc2804ba04f4e49d",
+    "url": "/static/js/main.6cce67b4.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
